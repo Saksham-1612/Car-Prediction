0d3bd1cba71b653ca006144551b0740338148399
Saksham Srivastava <saksham16dec@gmail.com>
2023-04-14 22:03:43 +0530

    updated

--- about.py
+++ about.py
@@ -3,11 +3,9 @@
 # Import necessary module
 import streamlit as st
 
+
 def app():
     st.balloons()
     st.title('Contact Us')
-    st.markdown('''### Name:
-    Shishir Shekhar''')
-    st.markdown('''### Email:
-    sspdav02@gmail.com''')
-    st.markdown('''### GitHub: [ShishirShekhar](https://github.com/ShishirShekhar/)''')
+    st.markdown('''### TEAM:
+    Saksham Srivastava | Rahgu Kowshik | Venkat''')
